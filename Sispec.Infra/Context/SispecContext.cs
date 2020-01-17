c5ac79e03eb277aef41ab6cece279fde5ee67ade
Mateus Tudeia <mateus.tudeia.lemaf@gmail.com>
2020-01-17 12:16:13 -0300

    Configuração do context para o Entity Framwork

---
+++ Sispec.Infra/Context/SispecContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sispec.Infra.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sispec.Infra.Context
+{
+    class SispecContext : DbContext
+    {
+        public DbSet<MPec> Pecs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host = 127.0.0.1; Database = dbsispec; Username = tudeia; Password = 123456");
+        
+    }
+}
