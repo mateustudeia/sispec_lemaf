5e9328cef81ace1d8d86ef90b3b0664b0f6397bf
Mateus Tudeia <mateustudeia@gmail.com>
2020-01-21 01:07:14 -0300

    Renomeada a entidade Evento p/ Entreterimento

---
+++ Sispec.Domain/Entities/Entreterimento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sispec.Domain.Entities
+{
+    public class Entreterimento : Evento
+    { 
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public int IdPessoa { get; set; }
+        public Pessoa Organizador { get; set; }
+
+    }
+}
