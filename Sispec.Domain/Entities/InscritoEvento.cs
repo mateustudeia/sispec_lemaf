74b9ced187d2ae93bd780f9243b44fe51af69c13
Mateus Tudeia <mateustudeia@gmail.com>
2020-01-21 01:10:54 -0300

    Entidade criada para configurar o relacionamento de entidades com o EF Core

---
+++ Sispec.Domain/Entities/InscritoEvento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sispec.Domain.Entities
+{
+    public class InscritoEvento
+    {
+        public int IdPessoa { get; set; }
+        public int IdEvento { get; set; }
+        public Evento Evento { get; set; }
+        public Pessoa Pessoa { get; set; }
+    }
+}
