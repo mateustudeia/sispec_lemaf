526417e07b3e23c4251519007fc98b14d096364e
Mateus Tudeia <mateus.tudeia.lemaf@gmail.com>
2020-02-04 15:33:50 -0300

    Revert "Revert "Merge branch 'front-end' into Correcao-Migracao""
    This reverts commit 1c384335146623e02023da81395bfd817e141152.

---
+++ Sispec.Application/Controllers/EventoControler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sispec.Domain.Entities;
+using Sispec.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sispec.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoControler : Controller
+    {
+        private SispecService<Evento> service = new SispecService<Evento>();
+
+        [HttpGet]
+        public ActionResult<IList<Evento>> Get()
+        {
+            return Ok(service.Get());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+            return Ok(service.Get(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Evento item)
+        {
+            service.Post(item);
+            return Ok("Adicionado!");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Evento item)
+        {
+
+            return Ok("Editado");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            service.Delete(id);
+            return new NoContentResult();
+        }
+    }
+}
