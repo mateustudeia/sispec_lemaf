57f090cb4cd29ce8daf36fe041cc69dce4922993
Mateus Tudeia <mateus.tudeia.lemaf@gmail.com>
2020-02-05 09:57:52 -0300

    Usando MVC

---
+++ Sispec.Service/PalestraService.cs
@@ -0,0 +1,12 @@
+﻿using Sispec.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sispec.Service
+{
+    class PalestraService : SispecService<Palestra>
+    {
+
+    }
+}
